de8a8c17eac854f5bd6b3866c7b92d75948bd7ba
Manohar  Singh <manoharsingh@Manohars-MacBook-Pro.local>
2019-11-12 20:57:41 +0530

    added building blocks

---
+++ StrategyPattern/Character.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace StrategyPattern
+{
+    public class Character
+    {
+        private IMoves _moves;
+
+        public Character(IMoves moves)
+        {
+            this._moves = moves;
+        }
+    }
+}
---
+++ StrategyPattern/IMoves.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StrategyPattern
+{
+    public interface IMoves
+    {
+        public void Attack();
+
+        public void Defend();
+
+        public void SpecialMove();
+    }
+}
